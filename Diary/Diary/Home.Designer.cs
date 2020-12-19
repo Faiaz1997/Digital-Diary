0fe08d7aa40576af3102ea9a64500e3f4c077544
Faiaz1997 <57425086+Faiaz1997@users.noreply.github.com>
2020-12-20 04:24:21 +0600

    Add files via upload

--- Diary/Diary/Home.Designer.cs
+++ Diary/Diary/Home.Designer.cs
@@ -28,20 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Event = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Priority = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Back = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.Modify = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Event,
+            this.Date,
+            this.Priority});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 472);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Event
+            // 
+            this.Event.HeaderText = "Event";
+            this.Event.Name = "Event";
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Priority
+            // 
+            this.Priority.HeaderText = "Priority";
+            this.Priority.Name = "Priority";
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(814, 379);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 2;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(733, 350);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(361, 12);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(517, 332);
+            this.text.TabIndex = 4;
+            // 
+            // Modify
+            // 
+            this.Modify.Location = new System.Drawing.Point(733, 379);
+            this.Modify.Name = "Modify";
+            this.Modify.Size = new System.Drawing.Size(75, 23);
+            this.Modify.TabIndex = 5;
+            this.Modify.Text = "Modify";
+            this.Modify.UseVisualStyleBackColor = true;
+            this.Modify.Click += new System.EventHandler(this.Modify_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(814, 350);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 6;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.button2_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(901, 496);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Modify);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Home";
             this.Text = "Home";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Home_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Event;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Priority;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Button Modify;
+        private System.Windows.Forms.Button Remove;
+
     }
 }
--- Diary/Diary/Home.cs
+++ Diary/Diary/Home.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Diary
 {
@@ -20,6 +21,47 @@ namespace Diary
         private void Home_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-V0HR07H\SQLEXPRESS;Initial Catalog=Diary;Integrated Security=True");
+            connection.Open();
+            string sql = "INSERT INTO [dbo].[event]([Text])VALUES('" + text.Text + "')";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+          if (result > 0)
+            {
+                MessageBox.Show("Text added successfully");
+                text.Text = string.Empty;
+            }
+            else
+                MessageBox.Show("Error in adding text.");
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            Welcome wc = new Welcome();
+            wc.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Modify_Click(object sender, EventArgs e)
+        {
+          
         }
     }
 }
